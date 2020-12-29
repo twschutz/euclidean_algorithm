e9cdf3d879f4831101aa79f64058b15117a61c84
twschutz <tedschutz3@gmail.com>
2020-12-29 15:58:40 -0400

    Added check for negative integers and added comments

--- euclidean2.cpp
+++ euclidean2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int num1;
 int num2;
 
+//REQUIRES: None
+//MODIFIES: num1, num2
+//EFFECTS: Prompts the user for input and stores the input
 void opener() {
     cout << "Welcome to the Euclidean Algorithm Generator!" << endl;
     cout << "Please enter the first number you would like to find the greatest common denominator of: ";
@@ -15,19 +18,28 @@ void opener() {
     cout << "Finding the greatest common denominator of " << num1 << " and " << num2 << "!" << endl;
 }
 
+//REQUIRES: num_in1 and num_in2 are non-negative
+//MODIFIES: None
+//EFFECTS: Recursively returns the greatest common divisor of the two inputs
 int findGcd(int num_in1, int num_in2) {
     cout << "Finding gcd(" << num_in1 << ", " << num_in2 << ")..." << endl;
+    //Base cases to return when remainder is 0
     if(num_in1 == 0) {
         return num_in2;
     }
     if(num_in2 == 0) {
         return num_in1;
     }
+    //Recursive case
     return findGcd(num_in2, (num_in1 % num_in2));
 }
 
 int main() {
     opener();
+    if(num1 < 0 || num2 < 0) {
+        cout << "Error! Trying to find the gcd of a negative number!" << endl;
+        return 1;
+    }
     int gcd = findGcd(num1, num2);
     cout << "The greatest common denominator is " << gcd << "!" << endl;
     return 0;
